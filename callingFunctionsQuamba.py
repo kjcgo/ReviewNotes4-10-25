98131d27541ba46023cd17051c2e190127b5c0e6
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-11 17:18:31 -0400

    Update callingFunctionsQuamba.py

--- callingFunctionsQuamba.py
+++ callingFunctionsQuamba.py
@@ -1,11 +1,18 @@
+#this is our main file
 def main():
 
+    #we are calling our sayHello function, meaning the computer will start executing statements 
+    #in that function
     sayHello()
 
+    #the program will return to the main function
     print("We are back in the main function")
 
 def sayHello():
+
+    #the computer will print these statements when the function is called
     print("Hello There!")
     print("This is inside the sayHello function.")
 
+    #once these statements have been executed, the program "returns" to the main function
 main()
