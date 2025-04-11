e809cc40106e984112f1a09989def9372ac49255
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-11 17:22:57 -0400

    Update returningValuesQuamba.py

--- returningValuesQuamba.py
+++ returningValuesQuamba.py
@@ -1,11 +1,16 @@
+#this is our main function
 def main():
 
+    #it will print the value returned by passing 1 and 2 to the sum function
     print(sum(1, 2))
 
-    
-
+#this is our sum function, it has two parameters- num1 and num2
 def sum(num1, num2):
+
+    #calculates the result of adding the two parameters
     result = num1 + num2
+
+    #returns the result 
     return result
 
 main()
