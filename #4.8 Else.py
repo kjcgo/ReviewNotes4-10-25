1ff36a1122b45d3d194a0da48788160bc09953e3
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-11 17:15:12 -0400

    Update #4.8 Else.py

--- #4.8 Else.py
+++ #4.8 Else.py
@@ -1,10 +1,18 @@
+#loop 3 times
 for n in range(3):
+
+    #if n does equal 5, which it won't, it will break out of the loop
     if n == 5:
         print("Breaking out of loop")
         break
+
+    #prints n after each iteration
     print(n)
 
+#if "break" does NOT execute, this else block will be executed
+#if "break" DOES execute, the else block will be skipped
 else:
     print(f'After the loop, n is {n}')
 
+#indicates outside of loop
 print("Hello!")
