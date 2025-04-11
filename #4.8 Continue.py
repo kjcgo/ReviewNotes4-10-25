fa0f84da5d59c50597f47bcb2dfda2ee7320841a
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-11 17:12:54 -0400

    Update #4.8 Continue.py

--- #4.8 Continue.py
+++ #4.8 Continue.py
@@ -1,12 +1,19 @@
+#initialize n
 n = 0
 
+#keep going until n < 10 is false
 while n < 10:    
+
+    #add one to n
     n += 1
 
+    #if n is divisible by 3, go back to the start of the loop
     if n % 3 == 0:
         continue
-    
+
+    #will print at the end of each iteration, is skipped if "continue" executes
     print(n)
-    
+
+#will print when the loop is complete
 print("We are done with the loop")
 
