220abea7488d9ca67eaf6cb1336930e0e2b73fa1
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-11 17:11:43 -0400

    Update #4.8 Break .py

--- #4.8 Break .py
+++ #4.8 Break .py
@@ -1,9 +1,15 @@
-n= 0
+#loops 10 times
 for x in range(10):
+
+    #asks for a number
     number = int(input("Enter a positive number"))
 
+    #will "break" out of the loop and proceed with the rest of the program.
     if number < 0:
         break
+
+    #will print at the end of each iteration
     print("This is inside of the for loop")
-    
+
+#will print once the loop has ended normally or "break" was executed
 print("This is outside of the for loop")
